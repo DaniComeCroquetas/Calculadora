410c64f8f702cfb926e189fbfaf4f839d5798656
DaniComeCroquetas <84933588+DaniComeCroquetas@users.noreply.github.com>
2021-05-27 21:16:28 +0200

    Add files via upload

---
+++ Calculadora.py
@@ -0,0 +1,14 @@
+num1 = input("Ingresa un número: ")
+num2 = input("Ingresa otro número: ")
+
+res = float(num1) + float(num2)
+res2 = float(num1) - float(num2)
+res3 = float(num1) / float(num2)
+res4 = float(num1) * float(num2)
+
+print("La suma es:", res)
+print("La resta es:", res2)
+print("La división es:", res3)
+print("La multiplicación es:", res4)
+
+print("Gracias!")
